3a68f88281cfdcdb3dffff2b5bed9626d527a246
juarezejp <juarez_ejp@live.com>
2019-09-19 13:08:14 -0500

    17

---
+++ 17.mvcmovie/Controllers/HelloWorldControllers.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace mvcmovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {   
+            ViewData["Message"] = "Hello " +name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            //return $"Hello {name}, NumTimes is: {numTimes}";
+        }
+    }
+}
